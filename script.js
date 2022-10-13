9389f61e0a7301f66b8723c955652974bb46c2c6
olareign <abdulrasaqolarewaju88@gmail.com>
2022-10-13 08:16:35 -1200

    modal

---
+++ script.js
@@ -0,0 +1,18 @@
+const openBtn = document.getElementById('open-btn');
+const modalContainer = document.getElementById('modal-container');
+const closeBtn = document.getElementById('close-btn');
+
+openBtn.addEventListener('click', function() {
+    modalContainer.style.display = "block";
+});
+
+closeBtn.addEventListener('click', function() {
+    modalContainer.style.display = "none";
+});
+
+// window.addEventListener('click', function(e) {
+//     if(e.target == modalContainer){
+//     modalContainer.style.display = "none";
+//     console.log("hey");
+//     }
+// });
